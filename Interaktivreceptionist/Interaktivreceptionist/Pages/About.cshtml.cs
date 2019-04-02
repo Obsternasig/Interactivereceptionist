e14cbcfd7e8121be44a51dc7bb12c9c43a788cd6
Obsternasig <36475440+Obsternasig@users.noreply.github.com>
2019-04-02 09:44:31 +0200

    gitignore

--- Interaktivreceptionist/Interaktivreceptionist/Pages/About.cshtml.cs
+++ Interaktivreceptionist/Interaktivreceptionist/Pages/About.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Interaktivreceptionist.Pages
 {
@@ -9,6 +10,19 @@ namespace Interaktivreceptionist.Pages
         public void OnGet()
         {
             Message = "Hej Bloggen, så skal der kneppes, høhø - prøv at skrive æ ø og å min pik er hoar ";
+        }
+    }
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace Kendo.Mvc.Examples.Controllers
+    {
+        public partial class ButtonController : Controller
+        {
+            [Demo]
+            public IActionResult Events()
+            {
+                return View();
+            }
         }
     }
 }
