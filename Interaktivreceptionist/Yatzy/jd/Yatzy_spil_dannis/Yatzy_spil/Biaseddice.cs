ba7adeb8846e36c4cf1a5e401f78f9f97b7c00b7
Dau17 <dennisau12@gmail.com>
2019-04-25 14:08:46 +0200

    super yatzyspillet (not)

---
+++ Interaktivreceptionist/Yatzy/jd/Yatzy_spil_dannis/Yatzy_spil/Biaseddice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzy_Mini_projekt
+{
+    public class BiasedDice : Dice
+    {
+        public BiasedDice(bool isFair)
+        {
+
+        }
+
+        new public void Roll()
+        {
+            // 
+        }
+        //For at holde på en værdi, lav det så terningen kun rolles når du skriver du skal rolle med den.
+    }
+}
