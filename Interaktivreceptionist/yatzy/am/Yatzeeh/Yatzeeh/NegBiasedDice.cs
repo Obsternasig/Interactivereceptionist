877978731ec0ca17c2d24567555f64d156f2df5b
Andreas <Andreas.troldborg@gmail.com>
2019-04-25 11:31:32 +0200

    yatzeeeeh

---
+++ Interaktivreceptionist/yatzy/am/Yatzeeh/Yatzeeh/NegBiasedDice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzeeh
+{
+    public class NegBiasedDice : Dice
+    {
+        public NegBiasedDice(bool isFair)
+        {
+
+        }
+
+        public int NegBiasedRoll()
+        {
+
+            var diceroll = Roll();
+            if (diceroll == 2)
+            {
+                return diceroll - 1;
+
+            }
+            if (diceroll >= 4)
+            {
+                return diceroll - 2;
+
+            }
+            return diceroll;
+        }
+    }
+}
---
+++ Interaktivreceptionist/yatzy/am/Yatzeeh/Yatzeeh/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzeeh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du har slået ");
+
+            var dice = new BiasedDice(true);
+            var negdice = new NegBiasedDice(true);
+
+            var diceRoll = dice.Roll();
+            var diceRoll2 = dice.Roll();
+            var diceRoll3 = dice.Roll();
+            var diceRoll4 = dice.Roll();
+            var diceRoll5 = dice.Roll();
+            var positiveDice = dice.BiasedRoll();
+            var negativeDice = negdice.NegBiasedRoll();
+
+            var sum = diceRoll + diceRoll2 + diceRoll3 + diceRoll4 + diceRoll5 + positiveDice + negativeDice;
+            Console.WriteLine($"Terning 1: {diceRoll} \nTerning 2: {diceRoll2} \nTerning 3: {diceRoll3} \nTerning 4: {diceRoll4} \nTerning 5: {diceRoll5} \npositivedice: {positiveDice} \nnegativedice {negativeDice}");
+            Console.WriteLine($"Den samlede sum af terningerne er {sum}");
+            Console.Read();
+        }
+    }
+}
