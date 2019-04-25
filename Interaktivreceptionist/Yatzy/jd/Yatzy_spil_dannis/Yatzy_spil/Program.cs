4a4ef11ebf10dead6e28e3aa25c58c2fa4458be7
JonasNymark <Jonasnymark86@gmail.com>
2019-04-25 14:09:50 +0200

    Ny

---
+++ Interaktivreceptionist/Yatzy/jd/Yatzy_spil_dannis/Yatzy_spil/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yatzy_spil;
+
+namespace Yatzy_Mini_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spilleklasse yatzy = new Spilleklasse(3);
+
+            yatzy.playRound();
+
+            Console.Write("Vælg dine terninger. OBS: VÆLGER DU EKSEMPELVIS TERNING 1, SÅ TAST TERNINGENS NUMMER OG VÆLGER DU FLERE SÅ ADSKIL VED KOMMA" );
+
+            char kast1 = (char)Console.Read();
+            if (Char.IsLetter(kast1))
+
+            }
+
+
+        }
+
+
+}
---
+++ Interaktivreceptionist/Yatzy/jd/Yatzy_spil_dannis/Yatzy_spil/yatzygameclass.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Yatzy_Mini_projekt;
+namespace Yatzy_spil
+{
+    public class Spilleklasse
+    {
+
+        List<Dice> activeDices;
+        private List<Dice> inactiveDices = new List<Dice>();
+        int diceThrows;
+        public Spilleklasse(int diceThrows)
+        {
+            activeDices = new List<Dice>();
+            this.diceThrows = diceThrows;
+            for(int i = 0; i < 6; i++)
+            {   if (i < 3)
+                    activeDices.Add(new Dice());
+                else
+                    activeDices.Add(new BiasedDice(false));
+            }
+            Console.WriteLine("Der er lige nu " + activeDices.Count + " aktive terninger");
+        }
+
+        public void playRound()
+        {
+            //For loop lige her på alle spillere
+            for(int i = 0; i < diceThrows; i++)
+
+            {
+                for(int j = 0; j < activeDices.Count; j++)
+                {
+                    activeDices[j].Roll();
+                    Console.WriteLine("Terning: " + (j + 1) + " slår " + activeDices[j].getValue());
+                }
+
+                // check er alle 1'ere
+                // check alle muligheder
+                // 
+
+                Console.ReadLine();
+
+            }
+        }
+
+    }
+
+}
