6e5d15c3078713fdf6ceaa5ed5c482b93001556c
JannikAAU <Jannie15@student.aau.dk>
2019-04-25 11:28:43 +0200

    Yatzy jd

---
+++ Interaktivreceptionist/Yatzy/jd/Yatzy_spil/Yatzy_spil/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yatzy_spil;
+
+namespace Yatzy_Mini_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spilleklasse yatzy = new Spilleklasse(3);
+
+            yatzy.playRound();
+
+            
+            
+            //Lav i main en metode der husker på hvad der er rollet med hvilket terninger. Efter et bruger har sagt hvilket terninger 
+            //de vil bruge, benyt en string split.
+        }
+
+    }
+}
