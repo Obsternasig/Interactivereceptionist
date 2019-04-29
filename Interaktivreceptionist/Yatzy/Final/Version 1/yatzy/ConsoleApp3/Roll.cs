04068bfc7ed157476222c2c8fa64b3eea9309554
Obsternasig <36475440+Obsternasig@users.noreply.github.com>
2019-04-30 00:52:28 +0200

    okokokok

---
+++ Interaktivreceptionist/Yatzy/Final/Version 1/yatzy/ConsoleApp3/Roll.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Roll
+    {
+        public List<IDie> Dice { get; set; } = new List<IDie>();
+
+        public Roll(int normalDice, int biasedDice)
+        {
+            for (int i = 0; i < normalDice; i++)
+            {
+                Dice.Add(new Die());
+            }
+            
+            for (int i = 0; i < biasedDice; i++)
+            {
+                Dice.Add(new BiasedDie(true));
+            }
+        }
+
+        public void RollDice(Random random)
+        {
+            foreach (IDie die in Dice)
+            {
+                if (!die.Locked)
+                    die.Roll(random);
+                die.Unlock();
+            }
+        }
+    }
+}
