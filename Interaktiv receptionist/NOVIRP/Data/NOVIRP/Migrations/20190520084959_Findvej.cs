b0d246e1b04ec2400df2a1d142d5c975e7af681b
JannikAAU <Jannie15@student.aau.dk>
2019-05-20 15:34:33 +0200

    Yes

---
+++ Interaktiv receptionist/NOVIRP/Data/NOVIRP/Migrations/20190520084959_Findvej.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NOVIRP.Migrations
+{
+    public partial class Findvej : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Findvej",
+                table: "NOVI",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Findvej",
+                table: "NOVI");
+        }
+    }
+}
