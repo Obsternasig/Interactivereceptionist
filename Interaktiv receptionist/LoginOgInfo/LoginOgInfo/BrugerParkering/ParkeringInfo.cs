5c53d02b08b9b0f6e12c97a8e1e758afc75a8bc8
JannikAAU <Jannie15@student.aau.dk>
2019-05-24 17:31:23 +0200

    Fredags arbejde

---
+++ Interaktiv receptionist/LoginOgInfo/LoginOgInfo/BrugerParkering/ParkeringInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginOgInfo.BrugerParkering
+{
+    public class ParkeringsInfo
+    {
+        public int ID { get; set; }
+        public string Nummerplade { get; set; } 
+    }
+}
