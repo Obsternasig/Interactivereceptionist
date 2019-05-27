533d8b5082e2a9bc36fd37b00716ed9eb6beb094
Obsternasig <36475440+Obsternasig@users.noreply.github.com>
2019-05-27 13:04:53 +0200

    css

--- Interaktiv receptionist/LoginOgInfo/LoginOgInfo/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Interaktiv receptionist/LoginOgInfo/LoginOgInfo/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -77,7 +77,7 @@ namespace LoginOgInfo.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
+                    return RedirectToPage("./AdminNavi");
                 }
                 if (result.RequiresTwoFactor)
                 {
